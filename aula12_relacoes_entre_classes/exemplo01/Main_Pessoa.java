26315fad6ca62be8d4cb18a5a9ab4819e8320575
JhonnyThomazz <jhonnythomazz@gmai.com>
2025-10-15 14:25:05 -0300

    Update 15/10/25

--- aula12_relacoes_entre_classes/exemplo01/Main_Pessoa.java
+++ aula12_relacoes_entre_classes/exemplo01/Main_Pessoa.java
@@ -3,10 +3,9 @@ package programação_orientada_a_objetos.aula12_relacoes_entre_classes.exemplo0
 public class Main_Pessoa {
 
 	public static void main(String[] args) {
-		Coracao coracao = new Coracao(190);
-		Pessoa pessoa = new Pessoa("Ralf Jones", 190);
+		Pessoa pessoa = new Pessoa("Ralf Jones", 80);
 		
-		System.out.println("Nome: " + pessoa.getNome() + "\nBatimentos por minuto(BPM): " + coracao.getBatimentos());
+		System.out.println("Nome: " + pessoa.getNome() + "\nBatimentos por minuto(BPM): " + pessoa.getCorcacao());
 
 	}
 
--- aula12_relacoes_entre_classes/exemplo01/Pessoa.java
+++ aula12_relacoes_entre_classes/exemplo01/Pessoa.java
@@ -2,18 +2,23 @@ package programação_orientada_a_objetos.aula12_relacoes_entre_classes.exemplo0
 
 public class Pessoa {
 
-		private String nome;
-		private Coracao coracao;//Composição_
-		
-		public Pessoa(String nome, int coracao) {
-			this.nome = nome;
-			this.coracao = new Coracao(coracao);
-		}
-		
-		public String getNome() {
-			return nome;
-		}
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+	private String nome;
+	private Coracao coracao;// Composição_
+
+	public Pessoa(String nome, int coracao) {
+		this.nome = nome;
+		this.coracao = new Coracao(coracao);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCorcacao() {
+		return coracao.getBatimentos();
+	}
 }
