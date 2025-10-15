d3f28dfa18127799ca3c5e1fa99cab5e7719d569
JhonnyThomazz <jhonnythomazz@gmai.com>
2025-10-15 10:02:48 -0300

    Update 15/10/25

---
+++ aula11_Polimorfismo/Atividade02/Medico.java
@@ -0,0 +1,23 @@
+package programação_orientada_a_objetos.aula11_Polimorfismo.Atividade02;
+
+public class Medico extends ProfissionalSaude{
+	private String crm;
+	
+	public Medico(String nome, String especialidade, int valorConsulta, String crm) {
+		super(nome, especialidade, valorConsulta);
+		this.crm = crm;
+	}
+	
+	public String getCrm() {
+		return crm;
+	}
+	public void setCrm() {
+		this.crm = crm;
+	}
+	
+	@Override
+	public void agendarConsulta() {
+		System.out.println("Agendando consulta...");
+		System.out.println("Check-up geral agendado para o dia: ");
+	}
+}
---
+++ aula11_Polimorfismo/Atividade02/Principal_Saude.java
@@ -0,0 +1,33 @@
+package programação_orientada_a_objetos.aula11_Polimorfismo.Atividade02;
+
+import java.util.Scanner;
+
+public class Principal_Saude {
+	
+	public static void main(String[] args) {
+		Medico medic = new Medico("Ralf Jones", "Ortopedista", 180, "123456/SP");
+		Dentista dentist = new Dentista("Terry Bogard", "Endodontia",200, "654321/SP");
+		Fisioterapeuta physicalTherapist = new Fisioterapeuta("Clark Still", "Neurofuncional",180, "132546-FN/SP" );
+		System.out.println("\n===Médico===");
+		System.out.println("Nome: " + medic.getNome());
+		System.out.println("Especialidade: " + medic.getEspecialidade());
+		System.out.println("Valor da consulta: " + medic.getValorConsulta());
+		System.out.println("CRM: " + medic.getCrm());
+		medic.agendarConsulta();
+		System.out.println("===================================");
+		System.out.println("\n===Dentista===");
+		System.out.println("Nome: " + dentist.getNome());
+		System.out.println("Especialidade: " + dentist.getEspecialidade());
+		System.out.println("Valor da consulta: " + dentist.getValorConsulta());
+		System.out.println("CRO: " + dentist.getCro());
+		dentist.agendarConsulta();	
+		System.out.println("===================================");
+		System.out.println("\n===Fisioterapeuta===");
+		System.out.println("Nome: " + physicalTherapist.getNome());
+		System.out.println("Especialidade: " + physicalTherapist.getEspecialidade());
+		System.out.println("Valor da consulta: " + physicalTherapist.getValorConsulta());
+		System.out.println("CREFITO: " + physicalTherapist.getCrefito());
+		physicalTherapist.agendarConsulta();	
+	
+	}
+}
